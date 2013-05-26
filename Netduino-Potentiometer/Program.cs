4f484b8228099fa3b976f74ac83c3a7ed0aaeba1
Scott Golightly <scott_golightly@hotmail.com>
2013-05-25 22:25:55 -0600

    Initial Code
    Code to turn the light on and off based on the settings of the
    potentiometer. I made a minor change from what is in the book to
    multiply the value of the potentiometer by 100 so it didn't always
    convert to 0 for the integer value to pass into Thread.Sleep.

---
+++ Netduino-Potentiometer/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Threading;
+using Microsoft.SPOT;
+using Microsoft.SPOT.Hardware;
+using SecretLabs.NETMF.Hardware;
+using SecretLabs.NETMF.Hardware.Netduino;
+
+namespace Netduino_Potentiometer
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            AnalogInput pot = new AnalogInput(Cpu.AnalogChannel.ANALOG_0);
+            OutputPort led = new OutputPort(Pins.GPIO_PIN_D0, false);
+
+            double potValue = 0.0;
+            int sleepValue = 0;
+
+            while (true)
+            {
+                potValue = pot.Read();
+                sleepValue = (int)(potValue * 100);
+                led.Write(true);
+                Thread.Sleep(sleepValue);
+                led.Write(false);
+                Thread.Sleep(sleepValue);
+            }
+        }
+
+    }
+}
